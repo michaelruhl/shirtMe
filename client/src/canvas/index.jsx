0bb1e5d3b55caf6762df675d50ad3c66454ff5b9
Michael Ruhl <michaelruhl14@hotmail.com>
2023-06-03 12:48:18 -0400

    import hdrfile

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -6,6 +6,10 @@ import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
 
+import hdrFile from "../assets/potsdamer_platz_1k.hdr";
+
+const preset = hdrFile;
+
 const CanvasModel = () => {
   return (
     <Canvas
@@ -15,7 +19,7 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
-      <Environment files="../assets/potsdamer_platz_1k.hdr" />
+      <Environment files={preset} />
 
       <CameraRig>
         <Backdrop />
